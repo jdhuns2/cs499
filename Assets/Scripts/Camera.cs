f3c8d7a2ea04721cf15d301bb3732e1af733f513
James Hunsucker <jdhuns2@uky.edu>
2013-02-26 14:32:35 -0500

    Fixed camera to that on start it checks the aspect ratio and sets itself such that (0,0) is in the bottom left corner of the screen

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -9,8 +9,29 @@ public class Camera : MonoBehaviour
     // Use this for initialization
     void Start()
     {// initial position for the camera. On my computer this results in (0,0,0) being the bottom left coordinate
-        //with the play area being in the +x, +y plane
-        transform.position = new Vector3(10.65f, 5.2f, -10);
+        //with the play area being in the +x, +y plane.  Since this depends on the screen aspect ratio there is case
+        //statement and if statement to check for repeating decimals ratios
+        switch (camera.aspect.ToString())
+        {case "1.25":
+                transform.position = new Vector3(6.55f, 5.2f, -10);
+                break;
+            case "1.5":
+                transform.position = new Vector3(7.85f, 5.2f, -10);
+                break;
+            case "1.6":
+                transform.position = new Vector3(8.35f, 5.2f, -10);
+                break;
+        }
+        if (camera.aspect > 1.30 && camera.aspect < 1.34)
+        {
+            transform.position = new Vector3(6.95f, 5.2f, -10);
+        }
+        if (camera.aspect > 1.70 && camera.aspect < 1.79)
+        {
+            transform.position = new Vector3(9.25f, 5.2f, -10);
+        }
+
+        //transform.position = new Vector3(10.65f, 5.2f, -10);
     }
 
     // Update is called once per frame
@@ -19,7 +40,7 @@ public class Camera : MonoBehaviour
         if (Input.GetKeyDown("space"))//debuging function
             //this will show the bottome left and top right screen coordinates in pixels
         {
-            Debug.Log(this.camera.aspect);
+            Debug.Log(this.camera.aspect.ToString());
             Debug.Log("botleft: " + camera.ViewportToWorldPoint(new Vector3(0, 0, camera.farClipPlane)));
             Debug.Log("topright: " + camera.ViewportToWorldPoint(new Vector3(1, 1, camera.farClipPlane)));
 
