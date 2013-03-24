4922c255471994bbf76fc881723129bfaf2d58ca
jakeBroaddus <jake.broaddus@uky.edu>
2013-03-24 14:13:57 -0400

    boosh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,7 +39,7 @@ public class Player : MonoBehaviour
             transform.position = new Vector3(transform.position.x, minY, transform.position.z);
         if (Input.GetKeyDown("space"))
         {
-
+			this.gameObject.SendMessage("Fire");
 
         }
     }
