f15b0328de3e3233b18315cf7a733360828a3355
jdhuns <jdhuns2@gmail.com>
2013-04-01 12:07:56 -0400

    Basic wave generation moved camera back

--- Assets/Scripts/WaveEmitter.cs
+++ Assets/Scripts/WaveEmitter.cs
@@ -5,6 +5,7 @@ public class WaveEmitter : MonoBehaviour {
 
 	RPathGen myPathgen;
 	EnemyManager myEnemyManager;
+	public int activeEnemies;
 	// Use this for initialization
 	void Start () {
 		GameObject go = (GameObject)GameObject.FindGameObjectWithTag("WaveGen");
@@ -20,7 +21,11 @@ public class WaveEmitter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if (activeEnemies<=0){
+			myEnemyManager.reAnimate ();
+			activeEnemies=myEnemyManager.basicAmount;
+			genPaths();
+		}
 	}
 	
 	
@@ -29,12 +34,13 @@ public class WaveEmitter : MonoBehaviour {
 	//Before each wave - create new paths 
 	void genPaths()
 	{
-		myPathgen.genPaths();
-	}
+		myPathgen.genPathsRange(5,10);
+	}	
 	
 	void newWave()
 	{
 		genPaths();
 		myEnemyManager.initEnemies();	//futue: here we pass the type of enemies
+		this.activeEnemies=10;
 	}
 }
---
+++ PathDirections.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AssemblyCSharp
+{
+	public struct PathDirections
+	{public float min, max;
+	}
+}
+
